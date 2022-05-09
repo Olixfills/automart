54adbbc840cfc047aab191dd34b022fdb48c002f
Olixfills <oniabrahamfills@gmail.com>
2022-05-10 00:37:12 +0100

    auth completed

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,61 @@
+import User from "../models/User-model";
+import bcrypt from 'bcryptjs'
+
+export const getAllUsers = async (req, res, next) => {
+    let users;
+
+    try {
+        users = await User.find()
+    } catch (err) {
+        return console.log(err);
+    }
+    if (!users) {
+        return res.status(404).json({message: 'No users found'})
+    }
+
+    return res.status(200).json({users: users});
+}
+
+
+
+export const signupUser = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const hashPass = bcrypt.hashSync(password)
+
+    const user = new User({
+        name,
+        email,
+        password: hashPass,
+    })
+    try {
+        await user.save()
+    } catch (err) {
+        return console.log(err.message);
+    }
+    return res.status(201).json({ user })
+    
+}
+
+export const loginUser = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({email})
+    } catch (err) {
+        console.log(err.message)
+    }
+    if (!existingUser) {
+        return res.status(404).json({ message: "No such user, please sign up instead"})
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+
+    if (!isPasswordCorrect) {
+        return res.status(400).json({message: "Incorrect Password"})
+    }
+
+    return res.status(200).json({message: "Login Successful"})
+
+}
---
+++ backend/routes/user-routes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { getAllUsers, loginUser, signupUser } from '../controllers/user-controller';
+
+
+const router = express.Router();
+
+
+router.get('/', getAllUsers)
+router.post('/signup',signupUser)
+router.post('/login',loginUser)
+
+
+
+
+export default router;
